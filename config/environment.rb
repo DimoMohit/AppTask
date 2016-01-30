3e7ce20c5d43af6d80f7377e74d043ab67f972c9
dimomohit99 <mohit@99tests.com>
2016-01-30 11:31:04 +0530

    SMTP Config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+if Rails.env == "production"
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'dimomohit@gmail.com',
+    :password => 'GANGAnilamDUTTA23!',
+    :domain => 'dimomohit.com',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
