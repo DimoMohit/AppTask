177b374b7d82d7ef3cf756989ba396f1537f5cc6
dimomohit99 <mohit@99tests.com>
2016-02-07 09:48:46 +0530

    User edit

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,7 +13,7 @@ class BlogsController < ApplicationController
     render :index
   end
   def create
-    blog=Blog.new(blog_params)
+    blog=current_user.blogs.build(blog_params)
     if blog.invalid?
       flash[:notice]="Unable to post because #{blog.error_message}"
       redirect_to :back
