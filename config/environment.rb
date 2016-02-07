ff557cc6948743be9f9f2bee2a1df233abfe4ed6
dimomohit99 <mohit@99tests.com>
2016-02-07 13:09:44 +0530

    Sendgrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,21 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 if Rails.env == "production"
   ActionMailer::Base.smtp_settings = {
-    :user_name => 'dimomohit@gmail.com',
-    :password => 'GITAnilamDUTTA23!',
+    :user_name => 'dimomohit10',
+    :password => 'dimo10MOHIT',
     :domain => 'dimomohit.com',
-    :address => 'smtp.gmail.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
+if Rails.env == "development"
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'dimomohit10',
+    :password => 'dimo10MOHIT',
+    :domain => 'dimomohit.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
