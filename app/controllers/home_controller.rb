a0fb19539fd68bb9639bddf991a751dc9cb7f3b0
dimomohit99 <mohit@99tests.com>
2016-06-14 00:42:31 +0530

    Sendgrid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def contact
     if params[:email].present? && params[:name].present? && params[:message].present?
-      Notifier.notify(params[:email],params[:name],params[:message])
+      Notifier.notify(params[:email],params[:name],params[:message]).deliver_now!
       flash[:info] = "Thanks for contacting us." 
       redirect_to root_path
     else
