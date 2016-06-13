d8b951cedb2c895ccd9b259a215a2f08ceaf7935
dimomohit99 <mohit@99tests.com>
2016-06-13 13:02:47 +0530

    Header and URL added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user! ,only: [:create,:new,:update,
     :edit,:destroy]
+  before_action :get_blog,only: [:edit,:update,:destroy]
   def index
     if params[:offset]
       @blogs=Blog.where(publish: true).offset(params[:offset]).first(10)
@@ -9,7 +10,7 @@ class BlogsController < ApplicationController
     end
   end
   def show
-    @blog=Blog.find_by_header(params[:id])||Blog.find(params[:id])
+    @blog=Blog.find_by_url(params[:id])||Blog.find(params[:id])
     render :index
   end
   def create
@@ -26,29 +27,34 @@ class BlogsController < ApplicationController
     end
   end
   def update
-    blog=current_user.blogs.find(params[:id])
-    blog.update_attributes(blog_params)
-    if blog.invalid?
+    @blog.update_attributes(blog_params)
+    if @blog.invalid?
       flash[:notice]="Unable to post because #{blog.error_message}"
       redirect_to :back
       return
     end
-    if blog.save
+    if @blog.save
       flash[:notice]="Thanks for posting the blog."
       redirect_to blogs_url
     end
   end
   def edit
-    @blog=current_user.blogs.find(params[:id])
     render "new"
   end
   def destroy
-    current_user.blogs.find(params[:id]).destroy
+    @blog.destroy
     redirect_to blogs_url
   end
   private
   def blog_params
       params.require(:blog).permit(:header,:sub_header,
         :blog_content,:publish,:url,:meta)
+  end
+  def get_blog
+    if current_user.is_admin?
+      @blog=Blog.find(params[:id])
+    else
+      @blog=current_user.blogs.find(params[:id])
+    end
   end
 end
